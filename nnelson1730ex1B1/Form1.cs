e60f71bf9f5e5e5494c1283f0d600702b2b9970b
Nicole PC <nicole.nelson@my.southeastmn.edu>
2021-08-25 14:31:24 -0500

    Add project files.

---
+++ nnelson1730ex1B1/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nnelson1730ex1B1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
