70da40298c754188a5b0a2d99efde3b95a08b869
Nicole PC <nicole.nelson@my.southeastmn.edu>
2021-08-27 23:42:22 -0500

    Created Ex1B

--- nnelson1730ex1B1/Program.cs
+++ nnelson1730ex1B1/Program.cs
@@ -16,7 +16,7 @@ namespace nnelson1730ex1B1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmInvoiceTotal());
         }
     }
 }
---
+++ nnelson1730ex1B1/frmInvoiceTotal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nnelson1730ex1B1
+{
+    public partial class frmInvoiceTotal : Form
+    {
+        public frmInvoiceTotal()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //txtTotal.Text = "10";
+            //txtTotal.ReadOnly = false;
+            
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
