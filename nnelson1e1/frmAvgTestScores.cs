1a3ce0deaa3d7c7e97aac6b6ffa1aca024a2428c
Nicole PC <nicole.nelson@my.southeastmn.edu>
2021-09-06 00:23:07 -0500

    ex1e2 complete

---
+++ nnelson1e1/frmAvgTestScores.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nnelson1e1
+{
+    public partial class frmAvgTestScores : Form
+    {
+        public frmAvgTestScores()
+        {
+            InitializeComponent();
+        }
+    }
+}
