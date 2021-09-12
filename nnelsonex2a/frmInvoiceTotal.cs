6cb7b01bba00d3768fbf6a1335186cc379bbbbf7
Nicole PC <nicole.nelson@my.southeastmn.edu>
2021-09-12 17:20:48 -0500

    Ex2a complete

---
+++ nnelsonex2a/frmInvoiceTotal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nnelsonex2a
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            decimal total = Convert.ToDecimal(txtSubtotal.Text);
+            decimal discount = Convert.ToDecimal(txtDiscountPercent.Text);
+            txtDiscountAmount.Text = ((total * discount) / 100).ToString("0.00");
+            txtTotal.Text = (total - Convert.ToDecimal(txtDiscountAmount.Text)).ToString("0.00");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
